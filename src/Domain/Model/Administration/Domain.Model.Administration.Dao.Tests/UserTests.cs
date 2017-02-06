e420a2f916d7de835a759a0512947c995963faf6
mmatveev <maxx.matveev@gmail.com>
2017-02-07 02:59:57 +0300

    [Feature] Administration.Domain.Tests. Add project

---
+++ src/Domain/Model/Administration/Domain.Model.Administration.Dao.Tests/UserTests.cs
@@ -0,0 +1,34 @@
+﻿using Askue.Domain.Model.Administration.Dao;
+using NUnit.Framework;
+
+namespace Domain.Model.Administration.Dao.Tests
+{
+	[TestFixture]
+	public class UserTests
+	{
+		#region fields
+
+		private UserDao _user;
+
+		#endregion
+
+		[OneTimeSetUp]
+		public void Init()
+		{
+			_user = new UserDao()
+			{
+				Name = "Максим",
+				LastName = "Матвеев",
+				Patronymic = "Евгеньевич"
+			};
+		}
+
+		[Test]
+		public void FullNameTest()
+		{
+			Assert.That(_user.FullName, 
+						Is.EqualTo(string.Format("{0} {1} {2}", _user.LastName, _user.Name, _user.Patronymic).Trim()));
+			Assert.Pass("Your first passing test");
+		}
+	}
+}
