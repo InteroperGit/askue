a3de2e1f79a496da92ead7eec5dafce1f9e5700b
mmatveev <maxx.matveev@gmail.com>
2017-02-07 02:59:00 +0300

    [Feature] Add Domain.Model.Shared project

---
+++ src/Infrastructure/Domain/Infrastructure.Domain.Model.Shared/EntityDao.cs
@@ -0,0 +1,19 @@
+﻿namespace Askue.Infrastructure.Domain.Model.Shared
+{
+	/// <summary>
+	/// Базовая DAO-сущность
+	/// </summary>
+	/// <typeparam name="T">Тип ключа сущности</typeparam>
+	public abstract class EntityDao<T>
+	{
+		#region properties
+
+		public virtual T Id { get; set; }
+
+		#endregion
+
+		#region methods
+
+		#endregion
+	}
+}
